246bf5959663fb9e1569fdfdd84c1d0f109ec4aa
rafaeltab <ikbenrafaelbieze2001@gmail.com>
2019-09-03 15:43:05 +0200

    Implemented_GameStateController_init

--- src/main/java/nl/quintor/solitaire/game/GameStateController.java
+++ src/main/java/nl/quintor/solitaire/game/GameStateController.java
@@ -1,13 +1,16 @@
 package nl.quintor.solitaire.game;
 
+import groovy.swing.factory.ColumnFactory;
+import nl.quintor.solitaire.models.card.Card;
+import nl.quintor.solitaire.models.card.Rank;
+import nl.quintor.solitaire.models.card.Suit;
 import nl.quintor.solitaire.models.deck.Deck;
 import nl.quintor.solitaire.models.deck.DeckType;
 import nl.quintor.solitaire.models.state.GameState;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 /**
  * Library class for GameState initiation and status checks that are called from {@link nl.quintor.solitaire.Main}.
@@ -24,8 +27,130 @@ public class GameStateController {
      * @return a new GameState object, ready to go
      */
     public static GameState init(){
-        // TODO: Write implementation
-        return new GameState();
+
+        //Create a default deck with all 52 cards (no jokers)
+        List<Card> allCards = new ArrayList<Card>();
+
+        for (int suit = 0; suit < 3; suit++) {
+            Suit s = Suit.CLUBS;
+            switch (suit) {
+                case 0:
+                    s = Suit.SPADES;
+                    break;
+                case 1:
+                    s = Suit.HEARTS;
+                    break;
+                case 2:
+                    s = Suit.CLUBS;
+                    break;
+                case 3:
+                    s = Suit.DIAMONDS;
+                    break;
+            }
+
+            //A = 0 J = 10 Q = 11 K = 12
+            for (int rank = 0; rank < 13; rank++) {
+                Rank r = Rank.ACE;
+                switch (rank) {
+                    case 0:
+                        r = Rank.ACE;
+                        break;
+                    case 1:
+                        r = Rank.TWO;
+                        break;
+                    case 2:
+                        r = Rank.THREE;
+                        break;
+                    case 3:
+                        r = Rank.FOUR;
+                        break;
+                    case 4:
+                        r = Rank.FIVE;
+                        break;
+                    case 5:
+                        r = Rank.SIX;
+                        break;
+                    case 6:
+                        r = Rank.SEVEN;
+                        break;
+                    case 7:
+                        r = Rank.EIGHT;
+                        break;
+                    case 8:
+                        r = Rank.NINE;
+                        break;
+                    case 9:
+                        r = Rank.TEN;
+                        break;
+                    case 10:
+                        r = Rank.JACK;
+                        break;
+                    case 11:
+                        r = Rank.QUEEN;
+                        break;
+                    case 12:
+                        r = Rank.KING;
+                        break;
+                }
+                allCards.add(new Card(s, r));
+            }
+        }
+
+        //shuffle the cards by switching 2 random ones 200 times
+        allCards = Shuffle(allCards,200);
+
+        //Create GameState instance
+        GameState state = new GameState();
+
+        //Create columns
+        int cardIndex = 0;
+        int column = 0;
+        String[] arr = {"A","B","C","D","E","F","G"};
+        for (var a : arr) {
+            Deck c = new Deck();
+            c.setInvisibleCards(column);
+            for(int card = 0; card < column+1;card++){
+                c.add(allCards.get(cardIndex));
+                cardIndex++;
+            }
+            state.getColumns().put(a,c);
+            column++;
+        }
+
+        //create stock
+        state.getStock().set(0,allCards.get(cardIndex));
+        cardIndex++;
+
+        //create waste
+        for (int w = 0 ; w < allCards.size()-cardIndex ; w++){
+            state.getWaste().set(w,allCards.get(cardIndex+w));
+        }
+
+        //Create stack
+        String[] arrr = {"A","B","C","D"};
+        for (var a: arrr ) {
+            state.getStackPiles().put(a,new Deck());
+        }
+
+        //return the state
+        return state;
+    }
+
+    private static List<Card> Shuffle(List<Card> deck, int iterations){
+        Random r = new Random();
+
+        for(int i = 0; i < iterations;i++){
+
+            var ind1 = r.nextInt(52);
+            var ind2 = r.nextInt(52);
+
+            Card c = deck.get(ind1);
+            deck.set(ind1,deck.get(ind2));
+            deck.set(ind2,c);
+
+        }
+
+        return deck;
     }
 
     /**
